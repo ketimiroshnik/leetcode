95f1d732536eb24bbc1b12b2bdfccbbd4318c96a
Kate Miroshnik <keti.miroshnik@gmail.com>
2025-03-15 00:21:04 +0300

    edit task 7

--- 7.py
+++ 7.py
@@ -1,25 +1,37 @@
 #49. Group Anagrams
 
-def groupAnagrams(strs):
-    """
-    :type strs: List[str]
-    :rtype: List[List[str]]
-    """
-    d = dict()
-    for i in range(len(strs)):
-        x = "".join(sorted(list(strs[i])))
-        if x in d:
-            d[x].append(i)
-        else:
-            d[x] = [i]
-    ans = []
-    for x in d:
-        mas = []
-        for index in d[x]:
-            mas.append(strs[index])
-        ans.append(mas)
-    return ans
+from collections import defaultdict
+
+class Solution(object):
+    def coding(self, s):
+        """
+        :param s: string of latin symbols
+        :return: string: символ в сллве + частота в алфавитном порядке
+        """
+        count = [0] * 26
+        for e in s:
+            count[ord(e) - ord('a')] += 1
+        res = []
+        for i in range(26):
+            if count[i] > 0:
+                res.append(chr(ord('a') + i))
+                res.append(str(count[i]))
+        return ''.join(res)
+
+
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        d = defaultdict(list)
+        for e in strs:
+            d[self.coding(e)].append(e)
+        ans = []
+        ans.extend(d.values())
+        return ans
 
 
 strs = ["eat","tea","tan","ate","nat","bat"]
-print(groupAnagrams(strs))
+s = Solution()
+print(s.groupAnagrams(strs))
